979d535ec7f79b1b35f9c361e4c44d005418e9b6
BibekaNayak <bibeka2020@gift.edu.in>
2024-08-20 13:57:22 +0530

    H_M_S

---
+++ src/main/java/com/org/controller/AdminController.java
@@ -0,0 +1,49 @@
+package com.org.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.org.dao.AdminDao;
+import com.org.dao.DoctorDao;
+import com.org.dto.Admin;
+import com.org.dto.Doctor;
+
+@Controller
+public class AdminController {
+
+	@Autowired
+	private AdminDao adminDao;
+
+	@PostMapping("/admin_register")
+	public ModelAndView admin_register(@ModelAttribute Admin admin) {
+		ModelAndView mav = new ModelAndView("admin_register.jsp");
+		adminDao.insertAndUpdateAdmin(admin);
+		mav.addObject("success", "Registered Succefully");
+		return mav;
+	}
+	
+	@PostMapping("/admin_login")
+	public ModelAndView adminLogin
+		(@RequestParam("email") String email,@RequestParam("password") String pwd) {
+		
+		List<Admin> list = adminDao.verifyAdminByEmailAndPassword(email, pwd);
+		if(list.isEmpty())
+		{
+			ModelAndView mav = new ModelAndView("admin_login.jsp");
+			mav.addObject("failed", "Invalid Email or Password");
+			return mav;
+		}
+		ModelAndView mav = new ModelAndView("admin/admin_home.jsp");
+		
+		return mav;
+	}
+}
---
+++ src/main/webapp/components/user_navbar.jsp
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#"><i
+			class="fa-solid fa-truck-medical"></i>Hospital</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent" aria-expanded="false"
+			aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<!-- User Navbar -->
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				
+				<li class="nav-item"><a class="nav-link active"
+					aria-current="page" href="temp_add_appointment"><i
+						class="fa-solid fa-hospital-user"></i>Add Appointment</a></li>
+				<li class="nav-item"><a class="nav-link active"
+					aria-current="page" href="view_apnmnt"><i
+						class="fa-solid fa-hospital-user"></i>View Appointment</a></li>
+			</ul>
+			
+		</div>
+
+	</div>
+</nav>
